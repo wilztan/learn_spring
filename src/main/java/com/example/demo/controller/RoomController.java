3a2ba0c6e27d994bc1c58d0cc35004d6e502fc16
Andrew Wiliam <awz77wil@gmail.com>
2019-04-27 22:42:41 +0700

    Adding fetchAvailableRooms endpoints

--- src/main/java/com/example/demo/controller/RoomController.java
+++ src/main/java/com/example/demo/controller/RoomController.java
@@ -9,12 +9,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/room")
 public class RoomController {
 
     @Autowired
     RoomService roomService;
+
+    @RequestMapping(method = RequestMethod.GET,path = "/available")
+    ResponseEntity<List<Room>> fetchAllAvailableRoom(){
+
+        List<Room> rooms = roomService.getAllAvailableRoom();
+
+        return  ResponseEntity.ok(rooms);
+    }
 
     @RequestMapping(method = RequestMethod.POST)
     ResponseEntity<Object> insertNewRoom(@RequestBody Room room){
@@ -24,4 +34,5 @@ public class RoomController {
                 result
         );
     }
+
 }
