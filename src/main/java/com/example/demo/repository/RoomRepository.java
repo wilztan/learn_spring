e9781154cbbb6175773ac30a3945a6a7e35ad109
Andrew Wiliam <awz77wil@gmail.com>
2019-04-27 22:34:54 +0700

    Adding findRoomsByStatusEquals for RoomRepository Business Logic

--- src/main/java/com/example/demo/repository/RoomRepository.java
+++ src/main/java/com/example/demo/repository/RoomRepository.java
@@ -3,5 +3,9 @@ package com.example.demo.repository;
 import com.example.demo.domain.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RoomRepository extends JpaRepository<Room,Long> {
+
+    public List<Room> findRoomsByStatusEquals(final boolean status);
 }
