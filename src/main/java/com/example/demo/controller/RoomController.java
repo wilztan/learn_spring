913c66f695ad854dea0021531be2fcbc3fbe12da
Andrew Wiliam <awz77wil@gmail.com>
2019-04-28 01:30:46 +0700

    Adding endPoints for removeRoom and updateRoom

--- src/main/java/com/example/demo/controller/RoomController.java
+++ src/main/java/com/example/demo/controller/RoomController.java
@@ -4,10 +4,7 @@ import com.example.demo.domain.Room;
 import com.example.demo.service.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,6 +26,24 @@ public class RoomController {
     @RequestMapping(method = RequestMethod.POST)
     ResponseEntity<Object> insertNewRoom(@RequestBody Room room){
         boolean result = roomService.insertNewRoom(room);
+
+        return ResponseEntity.ok(
+                result
+        );
+    }
+
+    @RequestMapping(method = RequestMethod.PUT)
+    ResponseEntity<Object> updateRoom(@RequestBody Room room, @RequestParam("roomId") long roomId){
+        boolean result = roomService.updateRoomDescription(room,roomId);
+
+        return ResponseEntity.ok(
+                result
+        );
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE)
+    ResponseEntity<Object> deleteRoom(@RequestParam("roomId") long roomId){
+        boolean result = roomService.removeRoom(roomId);
 
         return ResponseEntity.ok(
                 result
